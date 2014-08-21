fecabff53a4730b867fffeb51d78ea29bc8b8d7f
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2014-08-21 00:39:51 -0300

    active

--- js/bblme.js
+++ js/bblme.js
@@ -1,12 +1,3 @@
-// Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
 FB.login(function(response) {
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
